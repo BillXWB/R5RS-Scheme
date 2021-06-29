80b0b251410a6f67828019a88eaa121ec57a0674
Kirin <1370302499@qq.com>
2021-06-29 23:48:41 +0800

    ✨Feat(§2.3): 实现各数据类型的解析

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import System.Environment
+import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
@@ -15,6 +15,37 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right _ -> "Found value"
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many $ noneOf "\""
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many $ letter <|> digit <|> symbol
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
