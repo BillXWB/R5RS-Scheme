32712e0d928dbf9bfbb7bc9e7dd5bd4884fa54a3
Kirin <1370302499@qq.com>
2021-06-29 16:14:20 +0800

    ✨Feat(§1, Ex 2): 对命令行参数进行四则运算

--- src/main.hs
+++ src/main.hs
@@ -5,4 +5,17 @@ import System.Environment (getArgs)
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn $ "Hello, " ++ args !! 0 ++ " and " ++ args !! 1
+  print $ calc' args
+  where
+    calc' args = calc (args !! 0) (args !! 1) (args !! 2)
+
+calc :: String -> String -> String -> Float
+calc lhs' op rhs' =
+  let lhs = read lhs'
+      rhs = read rhs'
+   in case op of
+        "+" -> lhs + rhs
+        "-" -> lhs - rhs
+        "*" -> lhs * rhs
+        "/" -> lhs / rhs
+        _ -> error $ "invalid op: " ++ op
