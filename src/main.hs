598687e9350a36eafba04bc972260b87fe048c7a
Kirin <1370302499@qq.com>
2021-06-30 18:21:14 +0800

    ✨Feat(§2.3, Ex 4): 添加 2, 8, 16 进制整数支持

--- src/main.hs
+++ src/main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Numeric (readHex, readOct)
 import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -12,12 +13,16 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+  Right val ->
+    "Found value: "
+      ++ case val of
+        String s -> "String " ++ s
+        v -> show v
 
 data LispVal
   = Atom String
@@ -30,14 +35,14 @@ data LispVal
 
 parseString :: Parser LispVal
 parseString = do
-  char '"'
+  try $ char '"'
   x <- many $ noneOf ['\\', '"'] <|> parseEscape
   char '"'
   return $ String x
 
 parseEscape :: Parser Char
 parseEscape = do
-  char '\\'
+  try $ char '\\'
   c <- anyChar
   return $ case c of
     '\'' -> '\''
@@ -54,16 +59,50 @@ parseEscape = do
 
 parseAtom :: Parser LispVal
 parseAtom = do
-  first <- letter <|> symbol
+  first <- try $ letter <|> symbol
   rest <- many $ letter <|> digit <|> symbol
-  let atom = first : rest
-  return $ case atom of
-    "#t" -> Bool True
-    "#f" -> Bool False
-    _ -> Atom atom
+  return $ Atom (first : rest)
+
+parseBool :: Parser LispVal
+parseBool = do
+  try $ char '#'
+  (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
 
 parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= \val -> return $ Number (read val)
+parseNumber = parseBin <|> parseOct <|> parseDec <|> parseHex
+
+parseBin :: Parser LispVal
+parseBin = do
+  try $ string "#b"
+  x <- many1 $ oneOf "01"
+  let d = readBin x where readBin x = readBin' 0 x
+      readBin' v [] = v
+      readBin' v (x : xs) =
+        let v' = v * 2 + (if x == '1' then 1 else 0)
+         in readBin' v' xs
+  return $ Number d
+
+parseOct :: Parser LispVal
+parseOct = do
+  try $ string "#o"
+  x <- many1 octDigit
+  let [(d, _)] = readOct x
+  return $ Number d
+
+parseDec :: Parser LispVal
+parseDec = parseDec' <|> parseDec''
+  where
+    parseDec' = Number . read <$> many1 digit
+    parseDec'' = do
+      try $ string "#d"
+      parseDec'
+
+parseHex :: Parser LispVal
+parseHex = do
+  try $ string "#x"
+  x <- many1 hexDigit
+  let [(d, _)] = readHex x
+  return $ Number d
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom <|> parseString <|> parseNumber <|> parseBool
