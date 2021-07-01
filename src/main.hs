c121e1e56538fc68bce01751200314118234496c
Kirin <1370302499@qq.com>
2021-07-01 17:36:22 +0800

    ✨Feat(§3.3): 支持 list 中嵌套的整数四则运算
    不会处理弱类型的类型转换。quasiquote 和 unquoted 看不懂，尚不支持。

--- src/main.hs
+++ src/main.hs
@@ -229,8 +229,33 @@ parseExpr =
 
 eval :: LispVal -> LispVal
 eval (List [Atom "quote", val]) = val
-eval (List [Atom "quasiquote", val]) = error "not support"
-eval (List [Atom "unquoted", val]) = error "not support"
-eval val@(Vector _) = error "not support"
-eval val@(List _) = error "not support"
+eval (List [Atom "quasiquote", val]) = error "` is not support"
+eval (List [Atom "unquoted", val]) = error ", is not support"
+eval (List (Atom func : args)) = apply func $ map eval args
+eval val@(List _) = error $ "invalid form: " ++ show val
 eval val = val
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =
+  [ ("+", numericBinop (+)),
+    ("-", numericBinop (-)),
+    ("*", numericBinop (*)),
+    ("/", numericBinop div),
+    ("mod", numericBinop mod),
+    ("quotient", numericBinop quot),
+    ("remainder", numericBinop rem)
+  ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String s) =
+  let n = reads s :: [(Integer, String)]
+   in if null n then 0 else fst $ head n
+unpackNum (List [n]) = unpackNum n
+unpackNum val = error $ "invalid number: " ++ show val
